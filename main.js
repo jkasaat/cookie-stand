b30f1cf5a04f1431eebd4b1750f287a536f9e307
Jalaluddin Kasaat <jalaluddinkasaat@MacBook-Pro.local>
2018-07-07 01:20:12 -0400

    used constructors

--- main.js
+++ main.js
@@ -1,155 +1,74 @@
 
-let storeOne = {
-    name: 'CookieStand A',
-    product : 'Selling sandwich cookies',
-    customers: 200 +' customers',
-    employees: 5 + ' employees',
-    location: 'Located in Silver Spring',
-    createCustomer: function() {
-        console.log(this.product)
-    }
+let Store = function(storeName, cookie1, cookie2, cookie3, cookie4, cookie5, cookie6, cookie7, cookie8, cookie9) {
+    this.storeName = storeName
+    this.cok1 = cookie1;
+    this.cok2 = cookie2;
+    this.cok3 = cookie3;
+    this.cok4 = cookie4;
+    this.cok5 = cookie5;
+    this.cok6 = cookie6;
+    this.cok7 = cookie7;
+    this.cok8 = cookie8;
+    this.cok9 = cookie9;
 };
 
-let storeTwo = {
-    name: 'CookieStand B',
-    product : 'Selling chocolate cookies',
-    customers: 150 + ' customers',
-    employees: 4 + ' employees',
-    location: 'Located in Rockville',
-    createCustomer: function() {
-        console.log(this.product)
-    }
+let storeTime = new Store('Store Locations', 6 + ':00AM', 7 + ':00AM', 8 + ':00AM', 9 + ':00AM', 10 + ':00AM', 11 + ':00AM', 12 + ':00PM', 1 + ':00PM', 2 + ':00PM')
+let storeOne = new Store('Silver Spring Cookie Stand', 120, 150, 250, 300, 330, 450, 280, 205, 330 );
+let storeTwo = new Store('Rockville Cookie Stand', 85, 140, 235, 285, 306, 405, 270, 185, 308);
+let storeThree = new Store('White Oak Cookie Stand', 90, 165, 245, 295, 310, 415, 290, 165, 340);
+let storeFour = new Store('Takoma Park Cookie Stand', 95, 175, 255, 308, 318, 422, 295, 168, 370);
+
+let storeArray = [storeTime, storeOne, storeTwo, storeThree, storeFour]
+
+let elBody = document.getElementById('table-container')
+
+let elTable = document.createElement('table')
+
+elTable.setAttribute('id', 'cookie-table')
+elBody.appendChild(elTable)
+
+for(let i=0; i<storeArray.length; i++) {
+
+    let elRow = document.createElement('tr')
+    elTable.appendChild(elRow)
+
+    let elTh = document.createElement('th')
+    elTh.innerText = storeArray[i].storeName
+    elRow.appendChild(elTh)
+
+    let cookie1 = document.createElement('td')
+    cookie1.innerText = storeArray[i].cok1
+    elRow.appendChild(cookie1)
+
+    let cookie2 = document.createElement('td')
+    cookie2.innerText = storeArray[i].cok2
+    elRow.appendChild(cookie2)
+
+    let cookie3 = document.createElement('td')
+    cookie3.innerText = storeArray[i].cok3
+    elRow.appendChild(cookie3)
+
+    let cookie4 = document.createElement('td')
+    cookie4.innerText = storeArray[i].cok4
+    elRow.appendChild(cookie4)
+
+    let cookie5 = document.createElement('td')
+    cookie5.innerText = storeArray[i].cok5
+    elRow.appendChild(cookie5)
+
+    let cookie6 = document.createElement('td')
+    cookie6.innerText = storeArray[i].cok6
+    elRow.appendChild(cookie6)
+
+    let cookie7 = document.createElement('td')
+    cookie7.innerText = storeArray[i].cok7
+    elRow.appendChild(cookie7)
+
+    let cookie8 = document.createElement('td')
+    cookie8.innerText = storeArray[i].cok8
+    elRow.appendChild(cookie8)
+
+    let cookie9 = document.createElement('td')
+    cookie9.innerText = storeArray[i].cok9
+    elRow.appendChild(cookie9)
 };
-
-let storeThree = {
-    name: 'CookieStand C',
-    product : 'Sellig vanilla cookies',
-    customers: 300 + ' customers',
-    employees: 7 + ' employees',
-    location: 'Located in White Oak',
-    createCustomer: function() {
-        console.log(this.product)
-    }
-};
-
-let storeFour = {
-    name: 'CookieStand D',
-    product : 'Selling butter cookies',
-    customers: 300 + ' customers',
-    employees: 7 + ' employees',
-    location: 'Located in Takoma Park',
-    createCustomer: function() {
-        console.log(this.product)
-    }
-};
-
-
-let myBody = document.getElementById('StoreInfo')
-
-// storeOne
-
-let storeOneName = document.createElement('h1')
-myBody.appendChild(storeOneName)
-storeOneName.setAttribute('class', 'store')
-storeOneName.innerText = storeOne.name
-
-let storeOneProd = document.createElement('p')
-myBody.appendChild(storeOneProd)
-storeOneProd.setAttribute('class', 'store')
-storeOneProd.innerText = storeOne.product
-
-let storeOneCust = document.createElement('p')
-myBody.appendChild(storeOneCust)
-storeOneCust.setAttribute('class', 'store')
-storeOneCust.innerText = storeOne.customers
-
-let storeOneEmp = document.createElement('p')
-myBody.appendChild(storeOneEmp)
-storeOneEmp.setAttribute('class', 'store')
-storeOneEmp.innerText = storeOne.employees
-
-let storeOneLoc = document.createElement('p')
-myBody.appendChild(storeOneLoc)
-storeOneLoc.setAttribute('class', 'store')
-storeOneLoc.innerText = storeOne.location
-
-// storeTwo
-
-let storeTwoName = document.createElement('h1')
-myBody.appendChild(storeTwoName)
-storeTwoName.setAttribute('class', 'store')
-storeTwoName.innerText = storeTwo.name
-
-let storeTwoProd = document.createElement('p')
-myBody.appendChild(storeTwoProd)
-storeTwoProd.setAttribute('class', 'store')
-storeTwoProd.innerText = storeTwo.product
-
-let storeTwoCust = document.createElement('p')
-myBody.appendChild(storeTwoCust)
-storeTwoCust.setAttribute('class', 'store')
-storeTwoCust.innerText = storeTwo.customers
-
-let storeTwoEmp = document.createElement('p')
-myBody.appendChild(storeTwoEmp)
-storeTwoEmp.setAttribute('class', 'store')
-storeTwoEmp.innerText = storeTwo.employees
-
-let storeTwoLoc = document.createElement('p')
-myBody.appendChild(storeTwoLoc)
-storeTwoLoc.setAttribute('class', 'store')
-storeTwoLoc.innerText = storeTwo.location
-
-// storeThree
-
-let storeThreeName = document.createElement('h1')
-myBody.appendChild(storeThreeName)
-storeThreeName.setAttribute('class', 'store')
-storeThreeName.innerText = storeThree.name
-
-let storeThreeProd = document.createElement('p')
-myBody.appendChild(storeThreeProd)
-storeThreeProd.setAttribute('class', 'store')
-storeThreeProd.innerText = storeThree.product
-
-let storeThreeCust = document.createElement('p')
-myBody.appendChild(storeThreeCust)
-storeThreeCust.setAttribute('class', 'store')
-storeThreeCust.innerText = storeThree.customers
-
-let storeThreeEmp = document.createElement('p')
-myBody.appendChild(storeThreeEmp)
-storeThreeEmp.setAttribute('class', 'store')
-storeThreeEmp.innerText = storeThree.employees
-
-let storeThreeLoc = document.createElement('p')
-myBody.appendChild(storeThreeLoc)
-storeThreeLoc.setAttribute('class', 'store')
-storeThreeLoc.innerText = storeThree.location
-
-// storeFour
-
-let storeFourName = document.createElement('h1')
-myBody.appendChild(storeFourName)
-storeFourName.setAttribute('class', 'store')
-storeFourName.innerText = storeFour.name
-
-let storeFourProd = document.createElement('p')
-myBody.appendChild(storeFourProd)
-storeFourProd.setAttribute('class', 'store')
-storeFourProd.innerText = storeFour.product
-
-let storeFourCust = document.createElement('p')
-myBody.appendChild(storeFourCust)
-storeFourCust.setAttribute('class', 'store')
-storeFourCust.innerText = storeFour.customers
-
-let storeFourEmp = document.createElement('p')
-myBody.appendChild(storeFourEmp)
-storeFourEmp.setAttribute('class', 'store')
-storeFourEmp.innerText = storeFour.employees
-
-let storeFourLoc = document.createElement('p')
-myBody.appendChild(storeFourLoc)
-storeFourLoc.setAttribute('class', 'store')
-storeFourLoc.innerText = storeFour.location
